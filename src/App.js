c31ecba05e7c7ec6fbd75f0a2ccfa92b9e946ea9
Gustavo-PCzin\Gustavo <gugaksouza@gmail.com>
2020-10-31 20:33:41 -0300

    Busca Atualizada

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import api from './api';
   </li>
 ))}*/
 
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -27,8 +28,8 @@ class App extends Component {
 
  
   async componentDidMount() {
-    const response = await api.get();
-    this.setState({ rios: response.data });
+   // const response = await api.get();
+    //this.setState({ rios: response.data });
   }
 
   handleChange = (evento) => {
@@ -39,22 +40,34 @@ class App extends Component {
 
   handleSubmit = async (evento) => {    
     evento.preventDefault();
-    if (this.state.value !== "") {
-      this.results.infos = [];
-      this.results.state = false;
 
-      const arrayRios = this.state.rios.features;  
-      
-      arrayRios.forEach(rio => {
-       if(rio.attributes.NORIOCOMP === this.state.value){
-         this.results.state = true;
-         const stringResult = 'Frequencia: ' + rio.attributes.FREQUENCIA + ', Impacto: '+rio.attributes.IMPACTO+', Vulnerabilidade: ' + rio.attributes.VULNERABILIDADE;
-         this.results.infos.push(stringResult); 
-         this.results.nome = this.state.value;      
-       }
-      });
-      console.log(this.results.state);
-    }    
+    let river = '';
+    let base_url = 'https://portal1.snirh.gov.br/ana/rest/services/dados_abertos/Trechos_de_Curso_de_Agua_Inundaveis/FeatureServer/query?layerDefs=%5B%7B%22layerId%22%3A+0%2C+%22where%22%3A+%22NORIOCOMP%3D%27';
+    let base_url2 = '%27%22%2C+%22outfields%22%3A+%22*%22+%7D%5D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&time=&outSR=&havingClause=&gdbVersion=&historicMoment=&returnDistinctValues=false&returnGeometry=false&maxAllowableOffset=&returnIdsOnly=false&returnCountOnly=false&returnZ=false&returnM=false&geometryPrecision=&multipatchOption=xyFootprint&returnTrueCurves=false&resultOffset=&resultRecordCount=&sqlFormat=standard&f=pjson';
+    let URL_Request = '';
+    this.results.state = false;
+    this.results.infos = [];
+    if (this.state.value !== "") {
+      this.results.state = true;
+      river = this.state.value;
+      river = river.replace(" " , "+");
+      console.log(river);
+      URL_Request = base_url + river + base_url2;
+      console.log(URL_Request);
+      const response = await api.get(URL_Request);
+      console.log(response);
+
+      response.data.layers[0].features.forEach(rio=>{
+
+        
+        let info = 'Nome: ' + rio.attributes.NORIOCOMP +', Frequência: '+rio.attributes.FREQUENCIA+', Impacto: '+
+                    rio.attributes.IMPACTO + ', Vulnerabilidade: '+rio.attributes.VULNERABILIDADE ;
+                   
+        console.log(info);
+        this.results.infos.push(info);            
+
+      });    
+    }
     this.setState({});
   };
 
